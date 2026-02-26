f5da152e833ae75c3e6858d1bead4d7fc7804a43
Mathias-chtlrd <mathias.chatelard@etu.iut-tlse3.fr>
2026-02-26 09:03:57 +0100

    adding AuthDatabaseHandler

---
+++ Modele/AuthDatabaseHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace R401_APIequipedesport\Modele; 
+
+use Exception;
+use PDO;
+
+class AuthDatabaseHandler {
+    private static ?DatabaseHandler $instance = null;
+    private readonly PDO $linkpdo;
+    private readonly string $server;
+    private readonly string $db;
+    private readonly string $login;
+    private readonly string $mdp;
+    
+//database connexion for BD_equipe with admin account
+    private function __construct(){
+        try{
+            $this->server = "mysql-mathmams.alwaysdata.net";
+            $this->db = "mathmams_bd_r401";
+            $this->login = "mathmams_admin";
+            $this->mdp = "rootpwd";
+            $this->linkpdo=new PDO("mysql:host=".$this->server.";dbname=".$this->db,$this->login,$this->mdp);
+        }catch(Exception $e){
+            die("Erreur : ".$e->getMessage());
+        }
+    }
+
+    public static function getInstance(): DatabaseHandler
+    {
+        if (self::$instance == null) {
+            self::$instance = new DatabaseHandler();
+        }
+        return self::$instance;
+    }
+
+    public function pdo(): PDO {
+        return $this->linkpdo;
+    }
+}
